edf5a5dc9e40840591ce81f519190881948bef32
Chris <liqdfire@gmail.com>
2019-02-12 14:44:22 -0500

    skeleton

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace RedditMonitorBot
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var host = new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+              
+            }).Build();
+
+      host.Run();
+    }
+  }
+}
